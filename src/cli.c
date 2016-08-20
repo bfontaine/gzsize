6b811b9ea38726fc6be99f2f856494376b14efcc
Baptiste Fontaine <b@ptistefontaine.fr>
2016-08-20 13:25:43 +0200

    Add missing 'close' call

--- src/cli.c
+++ src/cli.c
@@ -85,6 +85,8 @@ int main(int argc, char **argv) {
 
                 fsize = get_size(fd);
 
+                close(fd);
+
                 if (fsize < 0) {
                         return fsize;
                 }
